c25cce47ec27b00401d2f38581f3a170e8cc3e11
Aurimas <nesvarbult2@gmail.com>
2024-03-18 10:39:38 +0200

    bug fixes again

--- UI/ui.gd
+++ UI/ui.gd
@@ -4,21 +4,28 @@ extends CanvasLayer
 var toggled := false
 
 signal spawn_item(item:String, state:bool)
+var prev_item
 
 
 
 func _on_berry_bush_toggled(toggled_on):
 	spawn_item.emit("BerryBush",toggled_on)
 	toggled=toggled_on
+	if toggled_on:
+		prev_item = "BerryBush"
 
 
 func _on_tree_toggled(toggled_on):
 	spawn_item.emit("Tree",toggled_on)
 	toggled=toggled_on
+	if toggled_on:
+		prev_item = "Tree"
 
 func _on_rock_toggled(toggled_on):
 	spawn_item.emit("Rock",toggled_on)
 	toggled=toggled_on
+	if toggled_on:
+		prev_item = "Rock"
 
 
 func _on_berry_bush_mouse_entered():
@@ -41,4 +48,12 @@ func _on_rock_mouse_entered():
 
 
 func _on_berry_bush_mouse_exited():
-	spawn_item.emit("BerryBush",toggled)
+	spawn_item.emit(prev_item,toggled)
+
+
+func _on_tree_mouse_exited():
+	spawn_item.emit(prev_item,toggled)
+
+
+func _on_rock_mouse_exited():
+	spawn_item.emit(prev_item,toggled)
