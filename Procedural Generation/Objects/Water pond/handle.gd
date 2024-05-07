e3e08f3cc2cd22336c04d379c7f92e05eeca5606
Silva <krikon1@ktu.lt>
2024-05-07 18:02:40 +0300

    fixes

---
+++ Procedural Generation/Objects/Water pond/handle.gd
@@ -0,0 +1,24 @@
+extends StaticBody3D
+@onready var amount : int = randi_range(1,4)
+var location : Vector3
+var weight : int
+
+var textmesh = TextMesh.new()
+
+func _ready():
+	textmesh.text = "water: %s" % [amount]
+	textmesh.font_size = 80
+	textmesh
+	$"../MeshInstance3D".mesh = textmesh
+
+func update() -> bool:
+	amount -= 1
+	if amount == 0:
+		queue_free()
+		return true
+	else:
+		textmesh.text = "water: %s" % [amount]
+		textmesh.font_size = 80
+		$"../MeshInstance3D".mesh = textmesh
+		return false
+	
---
+++ game_over.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _on_new_game_pressed():
+	Globals.LOADED = false
+	get_tree().change_scene_to_file("res://MainScene.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
+	
